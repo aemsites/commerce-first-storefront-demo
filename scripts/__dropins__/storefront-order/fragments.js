bb284c9dd8433aec23e4825b01d535d1c1958103
Miquel Angel Coca Piza <cocapiza@Miquels-MacBook-Pro.local>
2025-02-06 18:33:31 +0100

    Updated from new version

--- scripts/__dropins__/storefront-order/fragments.js
+++ scripts/__dropins__/storefront-order/fragments.js
@@ -1,67 +1,41 @@
-/*! Copyright 2025 Adobe
-All Rights Reserved. */
-const T=`
-  fragment REQUEST_RETURN_ORDER_FRAGMENT on Return {
-    __typename
-    uid
-    status
-    number
-    created_at
+const T = `fragment REQUEST_RETURN_ORDER_FRAGMENT on Return {
+  __typename
+  uid
+  status
+  number
+  created_at
+}`, e = `fragment ADDRESS_FRAGMENT on OrderAddress {
+  city
+  company
+  country_code
+  fax
+  firstname
+  lastname
+  middlename
+  postcode
+  prefix
+  region
+  region_id
+  street
+  suffix
+  telephone
+  vat_id
+}`, _ = `fragment PRODUCT_DETAILS_FRAGMENT on ProductInterface {
+  __typename
+  canonical_url
+  url_key
+  uid
+  name
+  sku
+  only_x_left_in_stock
+  stock_status
+  thumbnail {
+    label
+    url
   }
-`,e=`
-  fragment ADDRESS_FRAGMENT on OrderAddress {
-    city
-    company
-    country_code
-    fax
-    firstname
-    lastname
-    middlename
-    postcode
-    prefix
-    region
-    region_id
-    street
-    suffix
-    telephone
-    vat_id
-  }
-`,_=`
-  fragment PRODUCT_DETAILS_FRAGMENT on ProductInterface {
-    __typename
-    canonical_url
-    url_key
-    uid
-    name
-    sku
-    only_x_left_in_stock
-    stock_status
-    thumbnail {
-      label
-      url
-    }
-    price_range {
-      maximum_price {
-        regular_price {
-          currency
-          value
-        }
-      }
-    }
-  }
-`,t=`
-  fragment PRICE_DETAILS_FRAGMENT on OrderItemInterface {
-    prices {
-      price_including_tax {
-        value
-        currency
-      }
-      original_price {
-        value
-        currency
-      }
-      original_price_including_tax {
-        value
+  price_range {
+    maximum_price {
+      regular_price {
         currency
         value
       }
@@ -77,50 +51,7 @@ const T=`
       value
       currency
     }
-    product {
-      ...PRODUCT_DETAILS_FRAGMENT
-    }
-    ...PRICE_DETAILS_FRAGMENT
-  }
-`,a=`
-  fragment BUNDLE_ORDER_ITEM_DETAILS_FRAGMENT on BundleOrderItem {
-    ...PRICE_DETAILS_FRAGMENT
-    bundle_options {
-      uid
-      label
-      values {
-        uid
-        product_name
-      }
-    }
-  }
-`,E=`
-  fragment DOWNLOADABLE_ORDER_ITEMS_FRAGMENT on DownloadableOrderItem {
-    product_name
-    downloadable_links {
-      sort_order
-      title
-    }
-  }
-`,R=`
-  fragment ORDER_ITEM_FRAGMENT on OrderItemInterface {
-    ...ORDER_ITEM_DETAILS_FRAGMENT
-    ... on BundleOrderItem {
-      ...BUNDLE_ORDER_ITEM_DETAILS_FRAGMENT
-    }
-    ... on GiftCardOrderItem {
-      ...GIFT_CARD_DETAILS_FRAGMENT
-      product {
-        ...PRODUCT_DETAILS_FRAGMENT
-      }
-    }
-    ...DOWNLOADABLE_ORDER_ITEMS_FRAGMENT
-  }
-
-  ${E}
-`,i=`
-  fragment ORDER_SUMMARY_FRAGMENT on OrderTotal {
-    grand_total {
+    original_price_including_tax {
       value
       currency
     }
@@ -248,11 +179,7 @@ ${E}`, i = `fragment ORDER_SUMMARY_FRAGMENT on OrderTotal {
         tracking_number
       }
     }
-  }
-`,u=`
-  fragment RETURNS_FRAGMENT on Returns {
-    __typename
-    items {
+    order {
       number
       token
     }
@@ -272,9 +199,34 @@ ${E}`, i = `fragment ORDER_SUMMARY_FRAGMENT on OrderTotal {
       }
     }
   }
-`,c=`
-  fragment GUEST_ORDER_FRAGMENT on CustomerOrder {
-    email
+}`), c = `fragment GUEST_ORDER_FRAGMENT on CustomerOrder {
+  email
+  id
+  number
+  order_date
+  order_status_change_date
+  status
+  token
+  carrier
+  shipping_method
+  printed_card_included
+  gift_receipt_included
+  available_actions
+  is_virtual
+  items_eligible_for_return {
+    ...ORDER_ITEM_DETAILS_FRAGMENT
+  }
+  returns {
+    ...RETURNS_FRAGMENT
+  }
+  payment_methods {
+    name
+    type
+  }
+  applied_coupons {
+    code
+  }
+  shipments {
     id
     tracking {
       title
@@ -300,30 +252,44 @@ ${E}`, i = `fragment ORDER_SUMMARY_FRAGMENT on OrderTotal {
         }
       }
     }
-    payment_methods {
-      name
-      type
-    }
-    shipping_address {
-      ...ADDRESS_FRAGMENT
-    }
-    billing_address {
-      ...ADDRESS_FRAGMENT
-    }
-    items {
-      ...ORDER_ITEM_FRAGMENT
-    }
-    total {
-      ...ORDER_SUMMARY_FRAGMENT
-    }
   }
-  ${_}
-  ${t}
-  ${r}
-  ${n}
-  ${a}
-  ${i}
-  ${e}
-  ${u}
-  ${R}
-`;export{e as ADDRESS_FRAGMENT,a as BUNDLE_ORDER_ITEM_DETAILS_FRAGMENT,E as DOWNLOADABLE_ORDER_ITEMS_FRAGMENT,r as GIFT_CARD_DETAILS_FRAGMENT,c as GUEST_ORDER_FRAGMENT,n as ORDER_ITEM_DETAILS_FRAGMENT,R as ORDER_ITEM_FRAGMENT,i as ORDER_SUMMARY_FRAGMENT,t as PRICE_DETAILS_FRAGMENT,_ as PRODUCT_DETAILS_FRAGMENT,T as REQUEST_RETURN_ORDER_FRAGMENT,u as RETURNS_FRAGMENT};
+  payment_methods {
+    name
+    type
+  }
+  shipping_address {
+    ...ADDRESS_FRAGMENT
+  }
+  billing_address {
+    ...ADDRESS_FRAGMENT
+  }
+  items {
+    ...ORDER_ITEM_FRAGMENT
+  }
+  total {
+    ...ORDER_SUMMARY_FRAGMENT
+  }
+}
+${_}
+${t}
+${r}
+${n}
+${a}
+${i}
+${e}
+${u}
+${R}`;
+export {
+e as ADDRESS_FRAGMENT,
+a as BUNDLE_ORDER_ITEM_DETAILS_FRAGMENT,
+E as DOWNLOADABLE_ORDER_ITEMS_FRAGMENT,
+r as GIFT_CARD_DETAILS_FRAGMENT,
+c as GUEST_ORDER_FRAGMENT,
+n as ORDER_ITEM_DETAILS_FRAGMENT,
+R as ORDER_ITEM_FRAGMENT,
+i as ORDER_SUMMARY_FRAGMENT,
+t as PRICE_DETAILS_FRAGMENT,
+_ as PRODUCT_DETAILS_FRAGMENT,
+T as REQUEST_RETURN_ORDER_FRAGMENT,
+u as RETURNS_FRAGMENT
+};
